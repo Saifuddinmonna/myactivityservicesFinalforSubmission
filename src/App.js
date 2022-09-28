9863aa376e0bda5ac8133fc9db3436aa8a2fbf55
Saifuddinmonna <Saifuddinmonna@gmail.com>
2022-09-29 01:01:49 +0600

    design added

--- src/App.js
+++ src/App.js
@@ -14,10 +14,67 @@ function App() {
 
 	console.log(cartbody);
 	return (
-		<div className=" container">
-			{cartbody.map((cart) => (
-				<CartBody key={cart.id} cart={cart}></CartBody>
-			))}
+		<div className=" main-container container">
+			<div className=" activityscarts-maindiv ">
+				{cartbody.map((cart) => (
+					<CartBody key={cart.id} cart={cart}></CartBody>
+				))}
+			</div>
+			<div className="border warning p-2 m-2 rounded-2 shadow ">
+				<div>
+					<img src="" alt="" />
+					<div>
+						<h5> Saifuddin Ahammed Munna</h5>
+						<p>Mymensingh,Bangladesh</p>
+					</div>
+				</div>
+				<div>
+					<div>
+						{" "}
+						<p></p>
+					</div>
+					<div>
+						<p></p>
+					</div>
+					<div>
+						<p></p>
+					</div>
+				</div>
+
+				<div className="m-2 p-2 border border-light  rounded-3">
+					<p className=" text-bolder ">Add A Break </p>
+					<div className="m-2 p-2">
+						<span className="rounded-circle bg-light border m-2 p-1">
+							10s
+						</span>
+						<span className="rounded-circle bg-light border m-2 p-1">
+							20s
+						</span>
+						<span className="rounded-circle bg-light border m-2 p-1">
+							30s
+						</span>
+						<span className="rounded-circle bg-light border m-2 p-1">
+							40s
+						</span>
+						<span className="rounded-circle bg-light border m-2 p-1">
+							50s
+						</span>
+					</div>
+				</div>
+
+				<div className="m-2 p-2">
+					<h5>Exerxise Details</h5>
+					<div className="bg-light p-3 m-2">
+						<h5>Add a break </h5>
+						<p>00000</p>
+					</div>
+					<div className="bg-light p-3 m-2 rounded-3 border-info">
+						{" "}
+						<h5 className="bg-light">Exercise Details</h5>
+						<p>00000</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/components/CartBody/CartBody.js
+++ src/components/CartBody/CartBody.js
@@ -1,25 +1,27 @@
 import React from "react";
-import './CartBody.css'
+import "./CartBody.css";
 const CartBody = ({ cart }) => {
 	console.log("cartbody", cart.img);
 	return (
-		<div className="activityscart border p-2 m-2 rounded-3 shadow text-wrap text-">
-			<img
-				id="img-control"
-				className="img-fluid border rounded-3 border-light border-2 shadow"
-				src={cart.img}
-				alt=""
-			/>
-			<p className="name fw-bolder">{cart.name}</p>
-			<p className="about text-wrap overflow"> {cart.about}</p>
-			<p className="for-age fw-bolder">For Age : {cart.age}</p>
-			<p className="time-required fw-bolder">
-				Time required : {cart.time}
-			</p>
-			<button className="w-100 rounded-bottom btn btn-primary">
-				{" "}
-				Add to Cart
-			</button>
+		<div className="activityscarts-maindiv ">
+			<div className=" activityscart border p-2 m-2 rounded-3 shadow text-wrap position-relative d-flex flex-column justify-content-between">
+				<img
+					id="img-control"
+					className="img-fluid border rounded-3 border-light border-2 shadow"
+					src={cart.img}
+					alt=""
+				/>
+				<p className="name fw-bolder">{cart.name}</p>
+				<p className="about text-wrap overflow"> {cart.about}</p>
+				<p className="for-age fw-bolder">For Age : {cart.age}</p>
+				<p className="time-required fw-bolder">
+					Time required : {cart.time}
+				</p>
+				<button className="w-100 rounded-bottom btn btn-primary p  ">
+					{" "}
+					Add to Cart
+				</button>
+			</div>
 		</div>
 	);
 };
